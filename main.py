973d1daac89d4a2eb56d3d86f7f29e2aedc29031
Tim O'Brien <timothy.c.obrien99@gmail.com>
2020-06-06 23:39:14 -0400

    test

--- main.py
+++ main.py
@@ -18,8 +18,9 @@ def handle():
 
     # Send ${amount} to {name} ({email}) for {description}
     body_args = body.split(' ')
-
     # splitting at space [send, amount, to, name, (email), for, description]
+    # need to flatten body_args[6..] into one entry
+    body_args[6] = ''.join(body_args[6:])
     header = {"content-type": "application/json", "accept": "application/json", "authorization": AUTH}
     
     # literal_eval needed to interface with api with number literal
